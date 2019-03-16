a45f04d0e9b34ebccded01cc214fb54e8bc6b1b0
aryyya.xyz@gmail.com <29365565+aryyya@users.noreply.github.com>
2019-03-15 20:43:25 -0700

    Begin REST Framework integration.

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -1,24 +1,49 @@
-"""hello_django URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
 from django.contrib import admin
 from django.urls import path
+from menu.models import Origin, TeaItem
+from rest_framework import routers, serializers, viewsets
 
-from menu import views
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
 
+class OriginSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Origin
+        fields = ('city', 'state', 'country', 'latitude', 'longitude', 'grower_name', 'grower_notes')
+
+class TeaItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TeaItem
+        fields = ('origin', 'tasting_notes', 'year', 'leaves', 'price_bar', 'price_bowl', 'price_shot', 'price_retail')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class OriginViewSet(viewsets.ModelViewSet):
+    queryset = Origin.objects.all()
+    serializer_class = OriginSerializer
+
+class TeaItemViewSet(viewsets.ModelViewSet):
+    queryset = TeaItem.objects.all()
+    serializer_class = TeaItemSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'origins', OriginViewSet)
+router.register(r'tea-items', TeaItemViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('menu/', views.items)
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
