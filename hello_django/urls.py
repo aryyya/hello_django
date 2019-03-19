f4d7e5a290e4fb7a5a37d578dde8b6c09cb81509
aryyya.xyz@gmail.com <29365565+aryyya@users.noreply.github.com>
2019-03-18 22:47:21 -0700

    Basic path to view mapping tests.

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
 from django.http import HttpResponse
+from django.urls import path
 
 def index(request):
-    return HttpResponse('hello_django says: "Hello, world!"')
+    return HttpResponse('hello_django says "Hello, world!"')
+
+def greet(request, name):
+    return HttpResponse(f'Hello, {name}!')
 
 urlpatterns = [
     path('', index),
+    path('greet/<str:name>', greet),
     path('admin/', admin.site.urls),
     url(r'^menu/', include('menu_api.urls')),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework'))
