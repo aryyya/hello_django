5a60b0d01837caf66959fb09f7b63277b7a00662
aryyya.xyz@gmail.com <29365565+aryyya@users.noreply.github.com>
2019-03-18 22:38:43 -0700

    Add index greeting.

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('hello_django says: "Hello, world!"')
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     url(r'^menu/', include('menu_api.urls')),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework'))
