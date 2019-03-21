343fe5463bf6b556097e633eac1cef79a64116a4
aryyya.xyz@gmail.com <29365565+aryyya@users.noreply.github.com>
2019-03-21 15:56:47 -0700

    Add view that makes use of many-to-many model relationship to return some interesting information.

--- greeter/urls.py
+++ greeter/urls.py
@@ -5,5 +5,6 @@ app_name = 'greeter'
 
 urlpatterns = [
     path('help', views.help),
+    path('music', views.music, name='music'),
     path('<str:name>', views.greet, name='greet')
 ]
--- greeter/views.py
+++ greeter/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest, HttpResponseNotFound
 from django.urls import reverse
+from .models import Member
+from django.core.exceptions import ObjectDoesNotExist
 
 def help(request):
     return HttpResponseRedirect(reverse('greeter:greet', args=('user',), current_app=request.resolver_match.namespace))
 
 def greet(request, name):
     return HttpResponse(f'Hello, {name}!')
+
+def music(request):
+    musician_query = request.GET.get('musician', '')
+    band_query = request.GET.get('band', '')
+
+    if musician_query == '' or band_query == '':
+        return HttpResponseBadRequest('Error: Must provide query parameters "musician" and "band".')
+
+    try:
+        member = Member.objects.get(musician__name=musician_query, band__name=band_query)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f'Error: No information exists for {query["musician"]} from {query["band"]}.')
+
+    musician = member.musician
+    band = member.band
+
+    return HttpResponse(f'{musician.name} played {musician.instrument} in the band {band.name}, which had {band.member_set.count() - 1} other members.')
